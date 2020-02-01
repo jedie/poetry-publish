165e3c630e93efb4170cb516ddb5493f86228e19
JensDiemer <git@jensdiemer.de>
2020-02-01 23:18:26 +0100

    don't publish if code style wrong and use creole_readme option

--- poetry_publish/self.py
+++ poetry_publish/self.py
@@ -4,6 +4,7 @@ from creole.setup_utils import update_rst_readme
 
 import poetry_publish
 import poetry_publish.publish
+from poetry_publish.utils.subprocess_utils import verbose_check_call
 
 
 def update_poetry_publish_readme():
@@ -19,7 +20,10 @@ def publish_poetry_publish():
         Call this via:
             $ poetry run publish
     """
+    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
+
     poetry_publish.publish.poetry_publish(
         package_root=Path(poetry_publish.__file__).parent.parent,
         version=poetry_publish.__version__,
+        creole_readme=True  # don't publish if README.rst is not up-to-date
     )
