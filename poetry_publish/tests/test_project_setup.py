0f5994c2cda53d24838a738ec4b9e61e0c51b074
JensDiemer <git@jensdiemer.de>
2020-02-02 16:51:29 +0100

    make poetry_publish.tests.test_project_setup usable for external packages

--- poetry_publish/tests/test_project_setup.py
+++ poetry_publish/tests/test_project_setup.py
@@ -3,10 +3,14 @@
     :copyleft: 2020 by the poetry-publish team
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-
+import os
+import shutil
+import subprocess
 from pathlib import Path
 
 import poetry_publish
+
+PACKAGE_ROOT = Path(poetry_publish.__file__).parent.parent
 
 
 def assert_file_contains_string(file_path, string):
@@ -19,7 +23,7 @@ def assert_file_contains_string(file_path, string):
 
 def test_version(package_root=None, version=None):
     if package_root is None:
-        package_root = Path(poetry_publish.__file__).parent.parent
+        package_root = PACKAGE_ROOT
 
     if version is None:
         version = poetry_publish.__version__
@@ -41,3 +45,20 @@ def test_version(package_root=None, version=None):
         file_path=Path(package_root, 'pyproject.toml'),
         string=f'version = "{version}"'
     )
+
+
+def test_poetry_check(package_root=None):
+    if package_root is None:
+        package_root = PACKAGE_ROOT
+
+    poerty_bin = shutil.which('poetry')
+
+    output = subprocess.check_output(
+        [poerty_bin, 'check'],
+        universal_newlines=True,
+        env=os.environ,
+        stderr=subprocess.STDOUT,
+        cwd=str(package_root),
+    )
+    print(output)
+    assert output == 'All set!\n'
